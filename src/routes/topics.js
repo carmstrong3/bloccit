4bc06873ef110fc6a484a00b32ca02c5d5ed7baf
Clifford Armstrong III <clifford.armstrong.3@gmail.com>
2018-10-31 13:01:12 -0700

    fin 4.3.10-ex

--- src/routes/topics.js
+++ src/routes/topics.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-
-const topicController = require("../controllers/topicController")
+const topicController = require("../controllers/topicController");
+const validation = require("./validation");
 
 router.get("/topics", topicController.index);
 router.get("/topics/new", topicController.new);
-router.post("/topics/create", topicController.create);
+router.post("/topics/create", validation.validateTopics, topicController.create);
 
 module.exports = router;
