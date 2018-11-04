eef88deca9f70b6addf22ab1a3cc126ddaf57d20
Clifford Armstrong III <clifford.armstrong.3@gmail.com>
2018-11-03 18:57:12 -0700

    cannot read property 'getTableName' of undefined error

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -28,11 +28,16 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: "userId",
       as: "comments"
     });
+
+    User.hasMany(models.Vote, {
+      foreignKey: "userId",
+      as: "votes"
+    });
   };
 
   User.prototype.isAdmin = function() {
     return this.role === "admin";
   };
-
+  
   return User;
 };
